0a1b0859683210ae23a874c74630978547cf1dd9
Ryan Wails <ryanswails@gmail.com>
2022-05-06 10:06:09 -0400

    updating crypto prototype module

--- src/net/proto/upgen/crypto/prototype.rs
+++ src/net/proto/upgen/crypto/prototype.rs
@@ -1,14 +1,62 @@
 use crate::net::proto::upgen::crypto::{self, Decrypt, Encrypt};
 
 use bytes::Bytes;
+use std::rc::Rc;
+
+use rand::rngs::OsRng;
+use rand::RngCore;
+
+use salsa20::cipher::{KeyIvInit, StreamCipher};
+use salsa20::Salsa20;
+use x25519_dalek::{x25519, PublicKey, SharedSecret, StaticSecret};
+
+#[derive(Clone)]
+struct Cipher {
+    secret_key: [u8; 32],
+    nonce_gen: Rc<Salsa20>,
+}
+
+impl Cipher {
+    pub fn new(secret_key: [u8; 32]) -> Cipher {
+        let nonce = [0x0; 8];
+
+        Cipher {
+            secret_key: secret_key,
+            nonce_gen: Rc::new(Salsa20::new(&secret_key.into(), &nonce.into())),
+        }
+    }
+}
 
 #[derive(Clone)]
 pub struct CryptoModule {
-    // Not sure what's gonna go in here yet.
+    my_secret_key: StaticSecret,
+    my_public_key: Option<PublicKey>,
+    our_shared_secret_key: Option<[u8; 32]>,
 }
 
 impl CryptoModule {
-    fn new() {}
+    pub fn new() -> CryptoModule {
+        let mut key: [u8; 32] = [0; 32];
+        OsRng.fill_bytes(&mut key);
+
+        CryptoModule {
+            my_secret_key: StaticSecret::from(key),
+            my_public_key: None,
+            our_shared_secret_key: None,
+        }
+    }
+
+    pub fn produce_my_half_handshake(&mut self) -> [u8; 32] {
+        self.my_public_key = Some(PublicKey::from(&self.my_secret_key));
+        self.my_public_key.unwrap().to_bytes()
+    }
+
+    /*
+     * Produces the shared secret.
+     */
+    pub fn receive_their_half_handshake(&mut self, their_public_key: [u8; 32]) {
+        self.our_shared_secret_key = Some(x25519(self.my_secret_key.to_bytes(), their_public_key));
+    }
 }
 
 impl Encrypt for CryptoModule {
@@ -23,46 +71,29 @@ impl Decrypt for CryptoModule {
     }
 }
 
-/*
-pub enum ProtocolPhase {
-    Handshake1, // client to server
-    Handshake2, // server to client
-    Data,
-}
+#[cfg(test)]
+mod tests {
 
-#[derive(Clone)]
-pub struct ProtocolSpec {
-    frames: ProtocolFrames,
-}
+    use super::*;
 
-#[derive(Clone)]
-struct ProtocolFrames {
-    handshake1: OvertFrameSpec,
-    handshake2: OvertFrameSpec,
-    data: OvertFrameSpec,
-}
+    fn get_connected_pair() -> (CryptoModule, CryptoModule) {
+        let mut alice = CryptoModule::new();
+        let mut bob = CryptoModule::new();
 
-impl ProtocolSpec {
-    pub fn new(
-        handshake1: OvertFrameSpec,
-        handshake2: OvertFrameSpec,
-        data: OvertFrameSpec,
-    ) -> ProtocolSpec {
-        ProtocolSpec {
-            frames: ProtocolFrames {
-                handshake1,
-                handshake2,
-                data,
-            },
-        }
+        let alice_key = alice.produce_my_half_handshake();
+        let bob_key = bob.produce_my_half_handshake();
+
+        alice.receive_their_half_handshake(bob_key);
+        bob.receive_their_half_handshake(alice_key);
+
+        (alice, bob)
     }
 
-    pub fn get_frame_spec(&self, phase: ProtocolPhase) -> OvertFrameSpec {
-        match phase {
-            ProtocolPhase::Handshake1 => self.frames.handshake1.clone(),
-            ProtocolPhase::Handshake2 => self.frames.handshake2.clone(),
-            ProtocolPhase::Data => self.frames.data.clone(),
-        }
+    #[test]
+    fn test_key_exchange() {
+        let (alice, bob) = get_connected_pair();
+
+        assert_eq!(alice.our_shared_secret_key, bob.our_shared_secret_key);
+        assert!(!alice.our_shared_secret_key.is_none());
     }
 }
-*/
