8b236e68269ec21e400d7d07f43da4626a7f89e0
Ryan Wails <ryanswails@gmail.com>
2023-03-31 15:29:39 -0400

    parser cleaning, adding full formats

--- src/lang/parse/implementation.rs
+++ src/lang/parse/implementation.rs
@@ -17,31 +17,12 @@ fn print_type_of<T>(_: &T) {
 
 fn parse_numeric_type(p: &RulePair) -> NumericType {
     assert!(p.as_rule() == Rule::numeric_type);
-
-    match p.as_str() {
-        "u8" => NumericType::U8,
-        "u16" => NumericType::U16,
-        "u32" => NumericType::U32,
-        "u64" => NumericType::U64,
-        "i8" => NumericType::I8,
-        "i16" => NumericType::I16,
-        "i32" => NumericType::I32,
-        "i64" => NumericType::I64,
-        _ => panic!(),
-    }
+    p.as_str().parse().unwrap()
 }
 
 fn parse_primitive_type(p: &RulePair) -> PrimitiveType {
     assert!(p.as_rule() == Rule::primitive_type);
-
-    match p.as_str() {
-        "bool" => PrimitiveType::Bool,
-        "char" => PrimitiveType::Char,
-        _ => {
-            // Numeric type
-            PrimitiveType::Numeric(parse_numeric_type(&p.clone().into_inner().next().unwrap()))
-        }
-    }
+    p.as_str().parse().unwrap()
 }
 
 fn parse_positive_numeric_literal(p: &RulePair) -> usize {
@@ -52,7 +33,7 @@ fn parse_positive_numeric_literal(p: &RulePair) -> usize {
 fn parse_identifier(p: &RulePair) -> Identifier {
     assert!(p.as_rule() == Rule::identifier);
 
-    Identifier(p.as_str().to_string())
+    p.as_str().parse().unwrap()
 }
 
 fn parse_primitive_array(p: &RulePair) -> PrimitiveArray {
@@ -95,8 +76,8 @@ fn parse_array(p: &RulePair) -> Array {
     let p = p.clone().into_inner().next().unwrap();
 
     match p.as_rule() {
-        Rule::primitive_array => Array::Primitive(parse_primitive_array(&p)),
-        Rule::dynamic_array => Array::Dynamic(parse_dynamic_array(&p)),
+        Rule::primitive_array => parse_primitive_array(&p).into(),
+        Rule::dynamic_array => parse_dynamic_array(&p).into(),
         _ => panic!(),
     }
 }
@@ -132,6 +113,29 @@ fn parse_field(p: &RulePair) -> Field {
     Field {
         name: nv,
         dtype: tv,
+    }
+}
+
+fn parse_format(p: &RulePair) -> Format {
+    assert!(p.as_rule() == Rule::format);
+
+    let mut p = p.clone().into_inner();
+
+    let id = p.next().unwrap();
+    let id = parse_identifier(&id);
+
+    let mut fields = vec![];
+
+    let f = p.next().unwrap();
+    fields.push(parse_field(&f));
+
+    while let Some(f) = p.next() {
+        fields.push(parse_field(&f));
+    }
+
+    Format {
+        name: id,
+        fields,
     }
 }
 
@@ -182,7 +186,7 @@ mod tests {
     #[test]
     fn test_parse_primitive_type() {
         let test_cases = vec![
-            ("u8", PrimitiveType::Numeric(NumericType::U8)),
+            ("u8", NumericType::U8.into()),
             ("bool", PrimitiveType::Bool),
             ("char", PrimitiveType::Char),
         ];
@@ -207,10 +211,7 @@ mod tests {
 
     #[test]
     fn test_parse_primitive_array() {
-        let test_cases = vec![(
-            "[u8; 10]",
-            PrimitiveArray(PrimitiveType::Numeric(NumericType::U8), 10),
-        )];
+        let test_cases = vec![("[u8; 10]", PrimitiveArray(NumericType::U8.into(), 10))];
 
         test_rule_pair(
             test_cases.iter(),
@@ -221,7 +222,7 @@ mod tests {
 
     #[test]
     fn test_parse_size_of_op() {
-        let test_cases = vec![("x.size_of", UnaryOp::SizeOf(Identifier("x".to_string())))];
+        let test_cases = vec![("x.size_of", UnaryOp::SizeOf("x".parse().unwrap()))];
 
         test_rule_pair(test_cases.iter(), Rule::size_of_op, parse_sizeof_op);
     }
@@ -231,8 +232,8 @@ mod tests {
         let test_cases = vec![(
             "[u8; x.size_of]",
             DynamicArray(
-                PrimitiveType::Numeric(NumericType::U8),
-                UnaryOp::SizeOf(Identifier("x".to_string())),
+                NumericType::U8.into(),
+                UnaryOp::SizeOf("x".parse().unwrap()),
             ),
         )];
 
@@ -244,14 +245,15 @@ mod tests {
         let test_cases = vec![
             (
                 "[u8; 10]",
-                Array::Primitive(PrimitiveArray(PrimitiveType::Numeric(NumericType::U8), 10)),
+                PrimitiveArray(NumericType::U8.into(), 10).into(),
             ),
             (
                 "[u8; x.size_of]",
-                Array::Dynamic(DynamicArray(
-                    PrimitiveType::Numeric(NumericType::U8),
-                    UnaryOp::SizeOf(Identifier("x".to_string())),
-                )),
+                DynamicArray(
+                    NumericType::U8.into(),
+                    UnaryOp::SizeOf("x".parse().unwrap()),
+                )
+                .into(),
             ),
         ];
 
@@ -260,7 +262,7 @@ mod tests {
 
     #[test]
     fn test_name_value() {
-        let test_cases = vec![("NAME: Foo", Identifier("Foo".to_string()))];
+        let test_cases = vec![("NAME: Foo", "Foo".parse().unwrap())];
 
         test_rule_pair(test_cases.iter(), Rule::name_value, parse_name_value);
     }
@@ -270,11 +272,11 @@ mod tests {
         let test_cases = vec![
             (
                 "TYPE: u8",
-                Array::Primitive(PrimitiveArray(PrimitiveType::Numeric(NumericType::U8), 1)),
+                PrimitiveArray(NumericType::U8.into(), 1).into(),
             ),
             (
                 "TYPE: [i8; 10]",
-                Array::Primitive(PrimitiveArray(PrimitiveType::Numeric(NumericType::I8), 10)),
+                PrimitiveArray(NumericType::I8.into(), 10).into(),
             ),
         ];
 
@@ -286,11 +288,35 @@ mod tests {
         let test_cases = vec![(
             "{ NAME: Foo; TYPE: u8 }",
             Field {
-                name: Identifier("Foo".to_string()),
-                dtype: Array::Primitive(PrimitiveArray(PrimitiveType::Numeric(NumericType::U8), 1)),
+                name: "Foo".parse().unwrap(),
+                dtype: PrimitiveArray(NumericType::U8.into(), 1).into(),
             },
         )];
 
         test_rule_pair(test_cases.iter(), Rule::field, parse_field);
+    }
+
+    #[test]
+    fn test_format() {
+        let test_cases = vec![(
+            "DEFINE Handshake FIELDS \
+            {NAME: Foo; TYPE: u8}, \
+            {NAME: Bar; TYPE: [u32; 10]};",
+            Format{
+                name: "Handshake".parse().unwrap(),
+                fields: vec![
+                    Field {
+                        name: "Foo".parse().unwrap(),
+                        dtype: PrimitiveArray(NumericType::U8.into(), 1).into(),
+                    },
+                    Field {
+                        name: "Bar".parse().unwrap(),
+                        dtype: PrimitiveArray(NumericType::U32.into(), 10).into(),
+                    }
+                ]
+            }
+        )];
+
+        test_rule_pair(test_cases.iter(), Rule::format, parse_format);
     }
 }
--- src/lang/types.rs
+++ src/lang/types.rs
@@ -1,5 +1,17 @@
 #![allow(dead_code)]
 
+use std::convert::From;
+use std::str::FromStr;
+
+/*
+ * No error is possible. No way to construct an empty enum.
+ */
+#[derive(Debug)]
+pub enum NeverError {}
+
+#[derive(Debug)]
+pub struct ParseError;
+
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum NumericType {
     U8,
@@ -12,11 +24,50 @@ pub enum NumericType {
     I64,
 }
 
+impl FromStr for NumericType {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, ParseError> {
+        match s {
+            "u8" => Ok(NumericType::U8),
+            "u16" => Ok(NumericType::U16),
+            "u32" => Ok(NumericType::U32),
+            "u64" => Ok(NumericType::U64),
+            "i8" => Ok(NumericType::I8),
+            "i16" => Ok(NumericType::I16),
+            "i32" => Ok(NumericType::I32),
+            "i64" => Ok(NumericType::I64),
+            _ => Err(ParseError{}),
+        }
+    }
+}
+
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum PrimitiveType {
     Numeric(NumericType),
     Bool,
     Char,
+}
+
+impl FromStr for PrimitiveType {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, ParseError> {
+        match s {
+            "bool" => Ok(PrimitiveType::Bool),
+            "char" => Ok(PrimitiveType::Char),
+            _ => match s.parse::<NumericType>() {
+                Ok(n) => Ok(n.into()),
+                Err(e) => Err(e),
+            }
+        }
+    }
+}
+
+impl From<NumericType> for PrimitiveType {
+    fn from(item: NumericType) -> PrimitiveType {
+        PrimitiveType::Numeric(item)
+    }
 }
 
 #[derive(Copy, Clone, Debug, PartialEq)]
@@ -35,6 +86,14 @@ pub enum DataType {
 #[derive(Clone, Debug, PartialEq)]
 pub struct Identifier(pub String);
 
+impl FromStr for Identifier {
+    type Err = NeverError;
+
+    fn from_str(s: &str) -> Result<Self, NeverError> {
+        Ok(Identifier(s.to_string()))
+    }
+}
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum UnaryOp {
     SizeOf(Identifier),
@@ -52,10 +111,28 @@ pub enum Array {
     Dynamic(DynamicArray),
 }
 
+impl From<PrimitiveArray> for Array {
+    fn from(item: PrimitiveArray) -> Array {
+        Array::Primitive(item)
+    }
+}
+
+impl From<DynamicArray> for Array {
+    fn from(item: DynamicArray) -> Array {
+        Array::Dynamic(item)
+    }
+}
+
 #[derive(Clone, Debug, PartialEq)]
 pub struct Field {
     pub name: Identifier,
     pub dtype: Array,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Format {
+    pub name: Identifier,
+    pub fields: Vec<Field>,
 }
 
 pub trait StaticallySized {
