42d3ef213e5065019dcec684c7d746640eb786ab
Rob Jansen <jansen@cs.umn.edu>
2023-06-29 16:21:10 -0400

    Remove dead code

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -1,22 +1,2 @@
-use std::fmt;
-
 pub mod chacha;
 pub mod kdf;
-
-#[derive(Debug)]
-pub enum Error {
-    CryptFailure,
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Error::CryptFailure => write!(f, "Error encrypting/decrypting payload"),
-        }
-    }
-}
-
-// pub trait CryptoProtocol {
-//     fn encrypt(&mut self, data: Bytes) -> Result<Bytes, crypto::Error>;
-//     fn decrypt(&mut self, data: Bytes) -> Result<Bytes, crypto::Error>;
-// }
