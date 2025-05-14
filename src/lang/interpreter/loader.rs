b6f02104db4db00fec795ddb0e6c4b7e4becffa0
Rob Jansen <jansen@cs.umn.edu>
2025-05-14 16:56:03 -0400

    Fix bug in program loader to prevent deadlocks

--- src/lang/interpreter/loader.rs
+++ src/lang/interpreter/loader.rs
@@ -11,7 +11,7 @@ use super::ForwardingDirection;
 // Each forwarding direction concurrently runs a loader to step through the
 // task graph and asynchronously load new programs to execute the tasks.
 // Since sometimes only a single forwarding direction is active, we use the
-// `tokio::sync::Notify` facility to make sure each fowarding direction is
+// `tokio::sync::Notify` facility to make sure each forwarding direction is
 // awoken when a new task for its direction becomes available in the graph.
 
 #[derive(Clone)]
@@ -43,7 +43,23 @@ impl<T: TaskProvider + Send> Loader<T> {
     }
 
     pub async fn load(&mut self, direction: ForwardingDirection) -> anyhow::Result<Program> {
-        // Run the actual load operation if we are currently in an unloaded state.
+        loop {
+            // Make sure we are synced with the current round.
+            self.sync_tasks()?;
+
+            // Wait for my direction to be ready.
+            self.wait(direction).await;
+
+            // Defensive: we expect a task to be here, but in case the other direction
+            // raced us and the available task got unloaded, we just loop and wait again.
+            if let Some(task) = self.take_next_task(direction)? {
+                return Ok(Program::new(task));
+            }
+        }
+    }
+
+    /// Load the next round of tasks if we are currently in an unloaded state.
+    fn sync_tasks(&mut self) -> anyhow::Result<()> {
         match self.state_shared.lock() {
             Ok(mut state) => {
                 if state.out_loaded.is_none() && state.in_loaded.is_none() {
@@ -75,39 +91,30 @@ impl<T: TaskProvider + Send> Loader<T> {
             }
             Err(e) => bail!("Loader mutex was poisoned during load: {}", e.to_string()),
         };
-
-        // OK, now async wait for the next notify permit and task for our direction.
-        let task = self.wait(direction).await?;
-        Ok(Program::new(task))
+        Ok(())
     }
 
-    async fn wait(&mut self, direction: ForwardingDirection) -> anyhow::Result<Task> {
-        loop {
-            // Wait for the notify permit indicating a task is ready for us.
-            match direction {
-                ForwardingDirection::AppToNet => {
-                    self.out_notify.notified().await;
-                }
-                ForwardingDirection::NetToApp => {
-                    self.in_notify.notified().await;
-                }
-            };
-
-            // Take the available task out of shared state.
-            let maybe_task = match self.state_shared.lock() {
-                Ok(mut state) => match direction {
-                    ForwardingDirection::AppToNet => state.out_loaded.take(),
-                    ForwardingDirection::NetToApp => state.in_loaded.take(),
-                },
-                Err(e) => bail!("Loader mutex was poisoned during wait: {}", e.to_string()),
-            };
-
-            // Defensive: we expect the task to be here, but in case the other direction
-            // raced us and the available task got unloaded, we just loop and wait again.
-            if let Some(task) = maybe_task {
-                return Ok(task);
+    /// Wait for the notify permit indicating a task is ready for `direction`.
+    async fn wait(&mut self, direction: ForwardingDirection) {
+        match direction {
+            ForwardingDirection::AppToNet => {
+                self.out_notify.notified().await;
+            }
+            ForwardingDirection::NetToApp => {
+                self.in_notify.notified().await;
             }
         }
+    }
+
+    /// Take the next available task for `direction` out of shared state.
+    fn take_next_task(&mut self, direction: ForwardingDirection) -> anyhow::Result<Option<Task>> {
+        Ok(match self.state_shared.lock() {
+            Ok(mut state) => match direction {
+                ForwardingDirection::AppToNet => state.out_loaded.take(),
+                ForwardingDirection::NetToApp => state.in_loaded.take(),
+            },
+            Err(e) => bail!("Loader mutex was poisoned during wait: {}", e.to_string()),
+        })
     }
 
     pub fn unload(&mut self, program: Program) -> anyhow::Result<()> {
@@ -115,9 +122,14 @@ impl<T: TaskProvider + Send> Loader<T> {
         // graph and then unload both directions tasks to make sure we reload later.
         match self.state_shared.lock() {
             Ok(mut state) => {
+                // Store the last task id so we can step forward.
                 state.last_unloaded = Some(program.task_id());
+                // Reset the tasks since they might change in the next round.
                 state.out_loaded = None;
                 state.in_loaded = None;
+                // Wake up both sides to ensure that someone steps forward to resync.
+                self.in_notify.notify_one();
+                self.out_notify.notify_one();
             }
             Err(e) => bail!("Loader mutex was poisoned during unload: {}", e.to_string()),
         };
