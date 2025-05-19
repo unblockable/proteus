69861e75ea802c920a93759859a0fe6f79a21a4d
Rob Jansen <jansen@cs.umn.edu>
2025-05-19 15:47:43 -0400

    Add initial batch of ci jobs

--- build.rs
+++ build.rs
@@ -9,9 +9,11 @@ fn main() {
     let missing = integration_test_bin_deps
         .iter()
         .filter_map(|bin| {
+            // When linting, assume all tools are available.
+            println!("cargo:rustc-check-cfg=cfg(have_{bin})");
+            // When not lining, build tests for only the tools we have.
             if which(bin).is_ok() {
                 println!("cargo:rustc-cfg=have_{bin}");
-                println!("cargo:rustc-check-cfg=cfg(have_{bin})");
                 None
             } else {
                 Some(bin.to_string())
