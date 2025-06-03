40719bb57f851e90084b2c7225d29edea35fc8a9
Rob Jansen <jansen@cs.umn.edu>
2025-06-03 12:48:55 -0400

    Add generics to heap to simplify usage

--- src/lang/interpreter/memory.rs
+++ src/lang/interpreter/memory.rs
@@ -1,27 +1,85 @@
+use crate::lang::message::Message;
+use crate::lang::types::{ConcreteFormat, Identifier};
+use anyhow::{anyhow, bail};
+use bytes::Bytes;
 use std::collections::HashMap;
 
-use crate::lang::types::Identifier;
-
-pub struct Heap<T> {
-    mem: HashMap<Identifier, T>,
+#[derive(Debug)]
+pub enum HeapDataKind {
+    Bytes,
+    Format,
+    Message,
+    Number,
 }
 
-impl<T> Heap<T> {
+// Generates `From` impls so we can go from the inner value to an enum instance
+// of the associated variant.
+#[enum_from::enum_from]
+// Generates `TryFrom` impls so we can go from an instance of `HeapData` to the
+// inner value, or an error if the inner value has an unexpected type.
+#[enum_from::enum_try_from]
+pub enum HeapData {
+    Bytes(Bytes),
+    Format(ConcreteFormat),
+    Message(Message),
+    Number(u128),
+}
+
+impl HeapData {
+    pub fn kind(&self) -> HeapDataKind {
+        match self {
+            HeapData::Bytes(_) => HeapDataKind::Bytes,
+            HeapData::Format(_) => HeapDataKind::Format,
+            HeapData::Message(_) => HeapDataKind::Message,
+            HeapData::Number(_) => HeapDataKind::Number,
+        }
+    }
+}
+
+pub struct Heap {
+    mem: HashMap<Identifier, HeapData>,
+}
+
+impl Heap {
     pub fn new() -> Self {
         Self {
             mem: HashMap::new(),
         }
     }
 
-    pub fn insert(&mut self, addr: Identifier, data: T) -> Option<T> {
-        self.mem.insert(addr, data)
+    pub fn insert<T>(&mut self, addr: Identifier, data: T) -> anyhow::Result<()>
+    where
+        T: Into<HeapData>,
+    {
+        self.mem
+            .insert(addr.clone(), data.into())
+            .map_or(Ok(()), |_| {
+                bail!("Overwrote heap data at address '{addr:?}'")
+            })
     }
 
-    pub fn get(&self, addr: &Identifier) -> Option<&T> {
-        self.mem.get(addr)
+    pub fn get<'a, T>(&'a self, addr: &Identifier) -> anyhow::Result<T>
+    where
+        T: TryFrom<&'a HeapData>,
+    {
+        let data = self
+            .mem
+            .get(addr)
+            .ok_or(anyhow!("No heap data at address '{addr:?}'"))?;
+        T::try_from(data)
+            .map_err(|_| anyhow!("Heap data is not of the requested type '{:?}'", data.kind()))
     }
 
-    pub fn remove(&mut self, addr: &Identifier) -> Option<T> {
-        self.mem.remove(addr)
+    pub fn remove<T>(&mut self, addr: &Identifier) -> anyhow::Result<T>
+    where
+        T: TryFrom<HeapData>,
+    {
+        let data = self
+            .mem
+            .remove(addr)
+            .ok_or(anyhow!("No heap data at address '{addr:?}'"))?;
+        let kind = data.kind();
+        T::try_from(data)
+            .map_err(|_| anyhow!("Heap data is not of the requested type '{:?}'", kind))
     }
 }
--- src/lang/interpreter/vm.rs
+++ src/lang/interpreter/vm.rs
@@ -10,17 +10,14 @@ use crate::lang::interpreter::memory::Heap;
 use crate::lang::ir::bridge::{Task, TaskID};
 use crate::lang::ir::v1::{InstructionV1, ReadNetLength};
 use crate::lang::message::Message;
-use crate::lang::types::{ConcreteFormat, Identifier, PubkeyEncoding};
+use crate::lang::types::{Identifier, PubkeyEncoding};
 use crate::lang::Role;
 use crate::net::{Reader, Writer};
 
 pub struct VirtualMachine {
     task: Task,
     next_ins_index: usize,
-    bytes_heap: Heap<Bytes>,
-    format_heap: Heap<ConcreteFormat>,
-    message_heap: Heap<Message>,
-    number_heap: Heap<u128>,
+    heap: Heap,
 }
 
 impl VirtualMachine {
@@ -28,10 +25,7 @@ impl VirtualMachine {
         Self {
             task,
             next_ins_index: 0,
-            bytes_heap: Heap::new(),
-            format_heap: Heap::new(),
-            message_heap: Heap::new(),
-            number_heap: Heap::new(),
+            heap: Heap::new(),
         }
     }
 
@@ -56,13 +50,9 @@ impl VirtualMachine {
     ) -> anyhow::Result<()> {
         match &self.task.ins[self.next_ins_index] {
             InstructionV1::ComputeLength(args) => {
-                let msg = self
-                    .message_heap
-                    .get(&args.from_msg_heap_id)
-                    .ok_or(anyhow!("No msg on heap"))?;
+                let msg: &Message = self.heap.get(&args.from_msg_heap_id)?;
                 let len = msg.len_suffix(&args.from_field_id);
-                self.number_heap
-                    .insert(args.to_heap_id.clone(), len as u128);
+                self.heap.insert(args.to_heap_id.clone(), len as u128)?;
             }
             InstructionV1::ConcretizeFormat(args) => {
                 let aformat = args.from_format.clone();
@@ -78,7 +68,7 @@ impl VirtualMachine {
                     let payload_id = darrays.iter().find(|&id| id != padding_field_id).unwrap();
 
                     // We know the payload bytes are there...
-                    let payload_bytes = self.bytes_heap.get(payload_id).unwrap();
+                    let payload_bytes: &Bytes = self.heap.get(payload_id)?;
 
                     let payload_nbytes = payload_bytes.len();
 
@@ -88,56 +78,50 @@ impl VirtualMachine {
                     padding.resize_with(padding_nbytes, || 255);
                     let padding = bytes::Bytes::from(padding);
 
-                    self.bytes_heap.insert(padding_field_id.clone(), padding);
+                    self.heap.insert(padding_field_id.clone(), padding)?;
 
                     use crate::lang::types::ToIdentifier;
                     // FIXME(rwails) MEGA HACK
-                    self.number_heap
-                        .insert("__padding_len_on_heap".id(), padding_nbytes as u128);
+                    self.heap
+                        .insert("__padding_len_on_heap".id(), padding_nbytes as u128)?;
                 }
 
                 // Get the fields that have dynamic lengths, and compute what the lengths
                 // will be now that we should have the data for each field on the heap.
-                let concrete_bytes: Vec<(Identifier, Option<&Bytes>)> = aformat
+                let concrete_bytes: Vec<(Identifier, anyhow::Result<&Bytes>)> = aformat
                     .get_dynamic_arrays()
                     .iter()
                     .map(|id| {
                         (
                             id.clone(),
                             // self.bytes_heap.get(&id).unwrap().len(),
-                            self.bytes_heap.get(id),
+                            self.heap.get(id),
                         )
                     })
                     .collect();
 
                 let mut concrete_sizes: Vec<(Identifier, usize)> = vec![];
-                for (id, bytes_opt) in concrete_bytes {
-                    concrete_sizes.push((id, bytes_opt.ok_or(anyhow!("No concrete bytes"))?.len()))
+                for (id, bytes_res) in concrete_bytes {
+                    concrete_sizes.push((id, bytes_res?.len()))
                 }
 
                 // Now that we know the total size, we can allocate the full format block.
                 let cformat = aformat.concretize(&concrete_sizes);
 
                 // Store it for use by later instructions.
-                self.format_heap.insert(args.to_heap_id.clone(), cformat);
+                self.heap.insert(args.to_heap_id.clone(), cformat)?;
             }
             InstructionV1::CreateMessage(args) => {
                 // Create a message with an existing concrete format.
-                let cformat = self
-                    .format_heap
-                    .remove(&args.from_format_heap_id)
-                    .ok_or(anyhow!("No fmt on heap"))?;
+                let cformat = self.heap.remove(&args.from_format_heap_id)?;
                 let msg = Message::new(cformat);
 
                 // Store the message for use in later instructions.
-                self.message_heap.insert(args.to_heap_id.clone(), msg);
+                self.heap.insert(args.to_heap_id.clone(), msg)?;
             }
             InstructionV1::DecryptField(args) => {
                 // TODO way too much copying here :(
-                let msg = self
-                    .message_heap
-                    .get(&args.from_msg_heap_id)
-                    .ok_or(anyhow!("No msg on heap"))?;
+                let msg: &Message = self.heap.get(&args.from_msg_heap_id)?;
                 let ciphertext = msg
                     .get_field_bytes(&args.from_ciphertext_field_id)
                     .map_err(|_| anyhow!("No ciphertext bytes"))?;
@@ -160,14 +144,11 @@ impl VirtualMachine {
 
                 let mut buf = BytesMut::with_capacity(plaintext.len());
                 buf.put_slice(&plaintext);
-                self.bytes_heap
-                    .insert(args.to_plaintext_heap_id.clone(), buf.freeze());
+                self.heap
+                    .insert(args.to_plaintext_heap_id.clone(), buf.freeze())?;
             }
             InstructionV1::EncryptField(args) => {
-                let msg = self
-                    .message_heap
-                    .get(&args.from_msg_heap_id)
-                    .ok_or(anyhow!("No msg on heap"))?;
+                let msg: &Message = self.heap.get(&args.from_msg_heap_id)?;
                 let plaintext = msg
                     .get_field_bytes(&args.from_field_id)
                     .map_err(|_| anyhow!("No field bytes"))?;
@@ -179,44 +160,38 @@ impl VirtualMachine {
 
                     let mut buf = BytesMut::with_capacity(ciphertext.len());
                     buf.put_slice(&ciphertext);
-                    self.bytes_heap
-                        .insert(args.to_ciphertext_heap_id.clone(), buf.freeze());
+                    self.heap
+                        .insert(args.to_ciphertext_heap_id.clone(), buf.freeze())?;
 
                     let mut buf = BytesMut::with_capacity(mac.len());
                     buf.put_slice(&mac);
-                    self.bytes_heap
-                        .insert(args.to_mac_heap_id.as_ref().unwrap().clone(), buf.freeze());
+                    self.heap
+                        .insert(args.to_mac_heap_id.as_ref().unwrap().clone(), buf.freeze())?;
                 } else {
                     // We are doing unauthenticated encryption.
                     let ciphertext = forwarder.encrypt_unauth(&plaintext).unwrap();
                     let mut buf = BytesMut::with_capacity(ciphertext.len());
                     buf.put_slice(&ciphertext);
-                    self.bytes_heap
-                        .insert(args.to_ciphertext_heap_id.clone(), buf.freeze());
+                    self.heap
+                        .insert(args.to_ciphertext_heap_id.clone(), buf.freeze())?;
                 }
             }
             InstructionV1::GenRandomBytes(_args) => {
                 unimplemented!()
             }
             InstructionV1::GetArrayBytes(args) => {
-                let msg = self
-                    .message_heap
-                    .get(&args.from_msg_heap_id)
-                    .ok_or(anyhow!("No msg on heap"))?;
+                let msg: &Message = self.heap.get(&args.from_msg_heap_id)?;
                 let bytes = msg
                     .get_field_bytes(&args.from_field_id)
                     .map_err(|_| anyhow!("No field bytes"))?;
-                self.bytes_heap.insert(args.to_heap_id.clone(), bytes);
+                self.heap.insert(args.to_heap_id.clone(), bytes)?;
             }
             InstructionV1::GetNumericValue(args) => {
-                let msg = self
-                    .message_heap
-                    .get(&args.from_msg_heap_id)
-                    .ok_or(anyhow!("No msg on heap"))?;
+                let msg: &Message = self.heap.get(&args.from_msg_heap_id)?;
                 let num = msg
                     .get_field_unsigned_numeric(&args.from_field_id)
                     .map_err(|_| anyhow!("No field num"))?;
-                self.number_heap.insert(args.to_heap_id.clone(), num);
+                self.heap.insert(args.to_heap_id.clone(), num)?;
             }
             InstructionV1::InitFixedSharedKey(args) => {
                 let salt = "stupid stupid stupid";
@@ -234,12 +209,12 @@ impl VirtualMachine {
                     .recv(args.from_len.clone())
                     .await
                     .map_err(|e| anyhow!("ReadApp error {e}"))?;
-                self.bytes_heap.insert(args.to_heap_id.clone(), data);
+                self.heap.insert(args.to_heap_id.clone(), data)?;
             }
             InstructionV1::ReadNet(args) => {
                 let len = match &args.from_len {
                     ReadNetLength::Identifier(id) => {
-                        let num = self.number_heap.get(id).ok_or(anyhow!("No num on heap"))?;
+                        let num: &u128 = self.heap.get(id)?;
                         let val = *num as usize;
                         Range {
                             start: val,
@@ -247,7 +222,7 @@ impl VirtualMachine {
                         }
                     }
                     ReadNetLength::IdentifierMinus((id, sub)) => {
-                        let num = self.number_heap.get(id).ok_or(anyhow!("No num on heap"))?;
+                        let num: &u128 = self.heap.get(id)?;
                         let val = (*num as usize) - sub;
                         Range {
                             start: val,
@@ -255,11 +230,8 @@ impl VirtualMachine {
                         }
                     }
                     ReadNetLength::IdentifierMinusMinus((id, id_sub, sub)) => {
-                        let num = self.number_heap.get(id).ok_or(anyhow!("No num1 on heap"))?;
-                        let num2 = self
-                            .number_heap
-                            .get(id_sub)
-                            .ok_or(anyhow!("No num2 on heap"))?;
+                        let num: &u128 = self.heap.get(id)?;
+                        let num2: &u128 = self.heap.get(id_sub)?;
                         let val = (*num as usize) - (*num2 as usize) - sub;
                         Range {
                             start: val,
@@ -272,39 +244,24 @@ impl VirtualMachine {
                     .recv(len)
                     .await
                     .map_err(|e| anyhow!("ReadNet error {e}"))?;
-                self.bytes_heap.insert(args.to_heap_id.clone(), data);
+                self.heap.insert(args.to_heap_id.clone(), data)?;
             }
             InstructionV1::SetArrayBytes(args) => {
-                let bytes = self
-                    .bytes_heap
-                    .get(&args.from_heap_id)
-                    .ok_or(anyhow!("No bytes on heap"))?;
-                let mut msg = self
-                    .message_heap
-                    .remove(&args.to_msg_heap_id)
-                    .ok_or(anyhow!("No msg on heap"))?;
+                let mut msg: Message = self.heap.remove(&args.to_msg_heap_id)?;
+                let bytes = self.heap.get(&args.from_heap_id)?;
                 msg.set_field_bytes(&args.to_field_id, bytes)
                     .map_err(|_| anyhow!("No field bytes"))?;
-                self.message_heap.insert(args.to_msg_heap_id.clone(), msg);
+                self.heap.insert(args.to_msg_heap_id.clone(), msg)?;
             }
             InstructionV1::SetNumericValue(args) => {
-                let val = *self
-                    .number_heap
-                    .get(&args.from_heap_id)
-                    .ok_or(anyhow!("No num on heap"))?;
-                let mut msg = self
-                    .message_heap
-                    .remove(&args.to_msg_heap_id)
-                    .ok_or(anyhow!("No msg on heap"))?;
-                msg.set_field_unsigned_numeric(&args.to_field_id, val)
+                let mut msg: Message = self.heap.remove(&args.to_msg_heap_id)?;
+                let val: &u128 = self.heap.get(&args.from_heap_id)?;
+                msg.set_field_unsigned_numeric(&args.to_field_id, *val)
                     .map_err(|_| anyhow!("Cannot set field num"))?;
-                self.message_heap.insert(args.to_msg_heap_id.clone(), msg);
+                self.heap.insert(args.to_msg_heap_id.clone(), msg)?;
             }
             InstructionV1::WriteApp(args) => {
-                let msg = self
-                    .message_heap
-                    .remove(&args.from_msg_heap_id)
-                    .ok_or(anyhow!("No msg on heap"))?;
+                let msg: Message = self.heap.remove(&args.from_msg_heap_id)?;
                 let data = msg
                     .into_inner_field(&args.from_field_id)
                     .ok_or(anyhow!("No msg to bytes"))?;
@@ -314,10 +271,7 @@ impl VirtualMachine {
                     .map_err(|e| anyhow!("WriteApp error {e}"))?;
             }
             InstructionV1::WriteNet(args) => {
-                let msg = self
-                    .message_heap
-                    .remove(&args.from_msg_heap_id)
-                    .ok_or(anyhow!("No msg on heap"))?;
+                let msg: Message = self.heap.remove(&args.from_msg_heap_id)?;
                 let data = msg.into_inner();
                 forwarder
                     .send(data)
@@ -325,10 +279,7 @@ impl VirtualMachine {
                     .map_err(|e| anyhow!("WriteNet error {e}"))?;
             }
             InstructionV1::WriteNetTwice(args) => {
-                let msg = self
-                    .message_heap
-                    .remove(&args.from_msg_heap_id)
-                    .ok_or(anyhow!("No msg on heap"))?;
+                let msg: Message = self.heap.remove(&args.from_msg_heap_id)?;
 
                 let mut data: Bytes = msg.into_inner();
                 let more = data.split_off(args.len_first_write);
@@ -354,16 +305,10 @@ impl VirtualMachine {
             }
             InstructionV1::ReadKey(args) => {
                 // TODO: dead code?
-                let _msg = self
-                    .message_heap
-                    .get(&args.from_msg_heap_id)
-                    .ok_or(anyhow!("No msg on heap"))?;
+                let _msg: &Message = self.heap.get(&args.from_msg_heap_id)?;
             }
             InstructionV1::SaveKey(args) => {
-                let msg = self
-                    .message_heap
-                    .get(&args.from_msg_heap_id)
-                    .ok_or(anyhow!("No msg on heap"))?;
+                let msg: &Message = self.heap.get(&args.from_msg_heap_id)?;
 
                 let bytes = msg
                     .get_field_bytes(&args.from_field_id)
