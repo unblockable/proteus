ed905c72a9c600011b65c782cba38ac2850999e9
Rob Jansen <jansen@cs.umn.edu>
2025-06-23 10:12:37 -0400

    Enforce max length of socks address names

--- src/net/proto/socks/address.rs
+++ src/net/proto/socks/address.rs
@@ -14,7 +14,8 @@ pub enum Socks5Address {
 
 impl Socks5Address {
     #[cfg(test)]
-    pub fn from_name(name: String) -> Socks5Address {
+    pub fn from_name(mut name: String) -> Socks5Address {
+        name.truncate(255);
         Socks5Address::Name(name)
     }
 
@@ -29,8 +30,8 @@ impl Socks5Address {
                 IpAddr::V4(_) => 1 + 4,  // type + addr
                 IpAddr::V6(_) => 1 + 16, // type + addr
             },
-            Socks5Address::Name(name) => 1 + 1 + name.len(), // type + len + name
-            Socks5Address::Unknown => 1,                     // type
+            Socks5Address::Name(name) => 1 + 1 + name.len().min(255), // type + len + name
+            Socks5Address::Unknown => 1,                              // type
         }
     }
 }
@@ -55,9 +56,10 @@ impl Serialize<Socks5Address> for Socks5Address {
                 }
             },
             Socks5Address::Name(name) => {
+                let len = name.len().min(255);
                 buf.put_u8(0x03);
-                buf.put_u8(name.len() as u8);
-                buf.put_slice(name.as_bytes());
+                buf.put_u8(len as u8);
+                buf.put_slice(&name.as_bytes()[0..len]);
             }
             Socks5Address::Unknown => {
                 buf.put_u8(0x0);
@@ -98,6 +100,64 @@ impl Deserialize<Socks5Address> for Socks5Address {
                 (src.remaining() >= 2).then(|| src.get_u16())?,
             )))),
             _ => Some(Socks5Address::Unknown),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+
+    use super::*;
+
+    #[test]
+    fn valid() {
+        let addresses = vec![
+            Socks5Address::from_name(String::from("test.com")),
+            Socks5Address::from_addr(IpAddr::V4(Ipv4Addr::new(4, 3, 2, 1))),
+            Socks5Address::from_addr(IpAddr::V6(Ipv6Addr::new(8, 7, 6, 5, 4, 3, 2, 1))),
+        ];
+
+        for addr in addresses {
+            let mut buf = BytesMut::new();
+            buf.put(addr.serialize());
+
+            assert_eq!(
+                addr,
+                Socks5Address::deserialize(&mut Cursor::new(&buf)).unwrap()
+            );
+        }
+    }
+
+    #[test]
+    fn max_length() {
+        let addr = Socks5Address::from_name("a".repeat(255));
+        assert_eq!(addr.len(), 257);
+
+        let mut buf = BytesMut::new();
+        buf.put(addr.serialize());
+
+        assert_eq!(
+            addr,
+            Socks5Address::deserialize(&mut Cursor::new(&buf)).unwrap()
+        );
+    }
+
+    #[test]
+    fn too_long() {
+        // Long names are truncated to 255 bytes
+        for len in [256, 257, 258, 300, 1000] {
+            let addr = Socks5Address::from_name("a".repeat(len));
+            // includes 1 byte for type, 1 byte for len, 255 bytes for name
+            assert_eq!(addr.len(), 257);
+
+            let mut buf = BytesMut::new();
+            buf.put(addr.serialize());
+
+            assert_eq!(
+                addr,
+                Socks5Address::deserialize(&mut Cursor::new(&buf)).unwrap()
+            );
         }
     }
 }
