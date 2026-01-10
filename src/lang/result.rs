bfd58910676661a5e64fa631e457f519631ef06e
Rob Jansen <jansen@cs.umn.edu>
2026-01-10 13:33:38 -0500

    Easy eof error check

--- src/lang/result.rs
+++ src/lang/result.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 pub type Result<T> = std::result::Result<T, RuntimeError>;
 
 #[derive(thiserror::Error, Debug)]
@@ -8,4 +10,27 @@ pub enum RuntimeError {
     Anyhow(#[from] anyhow::Error),
     // #[error("File not found: {path}")]
     // NotFound { path: String, #[source] io_error: std::io::Error }, // Adds context and source
+}
+
+impl RuntimeError {
+    pub fn is_eof(&self) -> bool {
+        if let RuntimeError::Io(e) = self && e.kind() == io::ErrorKind::UnexpectedEof {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io;
+
+    use crate::lang::RuntimeError;
+
+    #[test]
+    fn is_eof_error() {
+        let error = RuntimeError::Io(io::ErrorKind::UnexpectedEof.into());
+        assert!(error.is_eof())
+    }
 }
